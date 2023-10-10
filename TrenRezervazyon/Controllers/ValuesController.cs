97ae7c75583b637be36860d726caf3f569d14a9f
yasirailyas <yasiraailyas@gmail.com>
2023-10-10 03:09:01 +0300

    Add project files.

---
+++ TrenRezervazyon/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrenRezervasyon;
+
+namespace TrenRezervasyonAPI.Controllers
+{
+    [ApiController]
+    [Route("api/rezervasyon")]
+    public class RezervasyonController : ControllerBase
+    {
+        [HttpPost("yap")]
+        public IActionResult YapRezervasyon([FromBody] RezervasyonIstegi istek)
+        {
+            var rezervasyonSonucu = RezervasyonuYap(istek);
+
+            if (rezervasyonSonucu.RezervasyonYapilabilir)
+            {
+                return Ok(rezervasyonSonucu);
+            }
+            else
+            {
+                return BadRequest(rezervasyonSonucu);
+            }
+        }
+
+        private RezervasyonSonucu RezervasyonuYap(RezervasyonIstegi istek)
+        {
+            var sonuc = new RezervasyonSonucu();
+            var kalanKisiSayisi = istek.RezervasyonYapilacakKisiSayisi;
+
+            for (int i = 0; i < istek.Tren.Vagonlar.Count; i++)
+            {
+                var vagon = istek.Tren.Vagonlar[i];
+                var kapasite = vagon.Kapasite;
+                var doluKoltukAdet = vagon.DoluKoltukAdet;
+
+                if (kalanKisiSayisi <= 0)
+                    break;
+
+                if (doluKoltukAdet < kapasite * 0.7)
+                {
+                    sonuc.RezervasyonYapilabilir = true;
+
+                    var bosKoltukSayisi = (int)(kapasite * 0.7 - doluKoltukAdet);
+                    var rezerveEdilenKisiSayisi = Math.Min(bosKoltukSayisi, kalanKisiSayisi);
+
+                    sonuc.YerlesimAyrinti.Add(new YerlesimAyrinti
+                    {
+                        VagonAdi = vagon.Ad,
+                        KisiSayisi = rezerveEdilenKisiSayisi
+                    });
+
+                    doluKoltukAdet += rezerveEdilenKisiSayisi;
+                    kalanKisiSayisi -= rezerveEdilenKisiSayisi;
+                }
+            }
+
+            if (kalanKisiSayisi > 0)
+                sonuc.RezervasyonYapilabilir = false;
+
+            return sonuc;
+        }
+
+
+
+    }
+
+}
---
+++ TrenRezervazyon/TrenRezervasyon.cs
@@ -0,0 +1,41 @@
+﻿namespace TrenRezervasyon
+{
+    public class Tren
+    {
+        public string Ad { get; set; }
+        public List<Vagon> Vagonlar { get; set; }
+    }
+
+    public class Vagon
+    {
+        public string Ad { get; set; }
+        public int Kapasite { get; set; }
+        public int DoluKoltukAdet { get; set; }
+    }
+
+    public class RezervasyonIstegi
+    {
+        public Tren Tren { get; set; }
+        public int RezervasyonYapilacakKisiSayisi { get; set; }
+        public bool KisilerFarkliVagonlaraYerlestirilebilir { get; set; }
+      
+    }
+
+
+    public class RezervasyonSonucu
+    {
+        public bool RezervasyonYapilabilir { get; set; }
+        public List<YerlesimAyrinti> YerlesimAyrinti { get; set; }
+
+        public RezervasyonSonucu()
+        {
+            YerlesimAyrinti = new List<YerlesimAyrinti>();
+        }
+    }
+
+    public class YerlesimAyrinti
+    {
+        public string VagonAdi { get; set; }
+        public int KisiSayisi { get; set; }
+    }
+}
